cada3946e45caf591d14aec7a7a78c3a205f2a09
Evgeny Cherkashin <eugeneai@aspire>
2010-06-20 16:12:44 +0900

    Fitting criterion changed to sigma.

--- global.R
+++ global.R
@@ -1,7 +1,6 @@
 # some definitions
 ZC=88
 FWHM=0.138
-sigma = FWHM / 2.35482
 sig_lim = 10
 SC = 20/(4096-zero_c)
 l5_9=5.92/sc
@@ -11,7 +10,8 @@ X = c(0:4095)
 Z = X * 0
 O = Z + 1
 
-gauss = function(x, x0=l5_9, A=1.0, sc=SC, zc=ZC) {
+gauss = function(x, x0=l5_9, A=1.0, sc=SC, zc=ZC, fwhm=FWHM) {
+    sigma = fwhm / 2.35482
     A*exp(-((x-(x0+zc))*sc)**2/(2*sigma**2))
 };
 
--- scaling.R
+++ scaling.R
@@ -2,34 +2,61 @@ source('global.R')
 
 STEP=FWHM/4.0/sc
 
-find_pike = function (start_x0, spectrum, step=STEP, channels=10*FWHM/sc, eps=1.0, plot=FALSE) {
+find_pike = function (start_x0, spectrum, step=STEP, channels=10*FWHM/sc, eps=1.0, plot=FALSE, fit_fwhm=TRUE) {
     x0 = start_x0
     term_x = x0 + channels
-    p_a = -1e38
+    p_sg = 1e38
     s = step
     f = list()
+    phase = 'raw'
     repeat {
         if (x0>term_x) {
            f$found=FALSE
            break;
         }
         g = gauss(X,x0=x0, zc=0, A=1, sc=sc)
-        #mm = mask(g, level=0.01)
-        f = lm(spectrum ~ g, weights=O)
-        a = f$coefficients[2]
-        b = f$coefficients[1]
-        if (b>0 && a>0){ 
-           if (a<p_a)
+        if (phase=='prec') {
+           mm = mask(g, level=0.001)
+           fm = spectrum ~ g + 0
+        } else {
+           mm = O
+           fm = spectrum ~ g
+        }
+        f = lm(fm, weights=mm)
+        cfs = f$coefficients
+        if (length(cfs)==2) { 
+           a = cfs[2] 
+           b = cfs[1]
+        } else {
+           a = cfs[1]
+           b = 0
+        }
+        #print (f)
+        sm = summary(f)
+        sg = sm$sigma
+        print (c(a, sg, x0, s, b))
+        if ((b>=0 || phase=='prec') && a>0){ 
+           if (sg > p_sg)
               s = -s/2.0
         }
         x0 = x0 + s
-        print (c(a, p_a, x0, s, b))
         #print (mm)
-        if (abs(a-p_a)<eps) {
-            f$found=TRUE
-            break;
+        if (a>0 && abs(sg-p_sg)<eps) {
+            if (phase=='prec') {
+               f$found=TRUE
+               f$mask = mm
+               f$sigma = sg
+               break;
+            }
+            phase='prec'
+            print ('change phase')
+            term_x=x0+step*2
+            s=step
+            x0=x0-s
+            p_sg = 1e38
+            next
         }
-        p_a = a
+        p_sg = sg
     }
     if (f$found) {
         
@@ -37,8 +64,43 @@ find_pike = function (start_x0, spectrum, step=STEP, channels=10*FWHM/sc, eps=1.
     f$x0 = x0
     f$A = a
     f$b = b
+    f$fwhm = FWHM
+    p_sg = 1e38
+    if (f$found && fit_fwhm) {
+       s = FWHM/8.0
+       fwhm = FWHM
+       print ('Fit fwhm')
+       repeat {
+           g = gauss(X,x0=x0, zc=0, A=1, sc=sc, fwhm=fwhm)
+           fm = spectrum ~ g # + 0
+           fw = lm (fm, weights = f$mask)
+           cfs = fw$coefficients
+           if (length(cfs)==2) { 
+              a = cfs[2] 
+              b = cfs[1]
+           } else {
+              a = cfs[1]
+              b = 0
+           }
+           sm = summary(fw)
+           sg = sm$sigma
+           if (sg > p_sg) {
+              s = -s/2.0
+           }
+           print (c(a, sg, p_sg, fwhm))
+           if (abs(sg-p_sg)<eps) {
+              f$fwhm = fwhm
+              f$A = a
+              f$b = b
+              f$sigma = sg
+              break
+           }
+           fwhm = fwhm + s
+           p_sg = sg
+       }
+    }
     if (plot && f$found) {
-        g = gauss(X, f$x0, A=f$A, zc=0., sc=SC)+f$b
+        g = gauss(X, f$x0, A=f$A, zc=0., sc=SC, fwhm=f$fwhm)+f$b
         plot(X, g, type='l')
         lines(X, spectrum, type='l', col='red')
     }
