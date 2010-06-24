dee9999832e375af0cac6cdb4ce87d92a8e66562
Evgeny Cherkashin at center <eugene@irnok.net>
2010-06-25 00:45:46 +0900

    Switched development go GTK+. Seems this is the right decision.

---
+++ app_gtk.py
@@ -0,0 +1,81 @@
+import pygtk
+pygtk.require('2.0')
+import gtk, sys
+
+
+import sys
+import gtk
+  
+class BuilderExample:
+
+    # Signal connection is linked in the glade XML file
+    def main_window_delete_event_cb(self, widget, data=None):
+        gtk.main_quit()
+    m_quit_activate_cb=main_window_delete_event_cb
+
+    # Signal connection is linked in the glade XML file
+    def on_button1_clicked(self, widget, data=None):
+        text = self.entry1.get_text()
+        self.label1.set_text(text)
+     
+    def __init__(self):
+        # Create a new Builder object
+        builder = gtk.Builder()
+        # Add the UI objects (widgets) from the Glade XML file
+        builder.add_from_file("ui/main_win_gtk.glade")
+        #builder.add_from_file("builder.ui")
+        
+        # Get objects (widgets) from the Builder
+        self.window = builder.get_object("main_window")
+        #self.entry1 = builder.get_object("entry1");
+        #self.label1 = builder.get_object("label1");
+        # Connect all singals to methods in this class
+        builder.connect_signals(self)
+        # Show the window and all its children
+        self.window.show_all()
+
+    def on_file_new(self, widget, data=None):
+        print "Created"
+
+    def on_file_open(self, widget, data=None):
+        print "Opened:", self.get_open_filename()
+
+    def get_open_filename(self):
+        
+        filename = None
+        chooser = gtk.FileChooserDialog("Open Project...", self.window,
+                                        gtk.FILE_CHOOSER_ACTION_OPEN,
+                                        (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
+                                         gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+
+        ffilter = gtk.FileFilter()
+        ffilter.add_pattern("*.")
+        #print gtk.FileChooserDialog.__doc__
+        
+        response = chooser.run()
+        if response == gtk.RESPONSE_OK:
+            filename = chooser.get_filename()
+        chooser.destroy()
+        return filename
+    
+    def error_message(self, message):
+    
+        # log to terminal window
+        # print message
+        
+        # create an error message dialog and display modally to the user
+        dialog = gtk.MessageDialog(None,
+                                   gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                                   gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, message)
+        
+        dialog.run()
+        dialog.destroy()
+
+
+def main():
+    gtk.main()
+    return
+    
+if __name__ == "__main__":
+    BuilderExample()
+    gtk.main()
