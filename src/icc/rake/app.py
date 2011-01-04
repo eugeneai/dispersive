78674966f53d7d96dc0f89471c86d916aa618682
Evgeny Cherkashin at center <eugene@irnok.net>
2011-01-04 23:43:22 +0800

    First implementation of config.

--- src/icc/rake/app.py
+++ src/icc/rake/app.py
@@ -2,10 +2,13 @@ from zope.configuration.xmlconfig import xmlconfig
 import zope.component as ZC
 from pkg_resources import resource_stream, resource_string
 import sys
+import cfgparse as cfg
 
 def main(package=None):
     if package == None:
         package=__name__
+    c=cfg.ConfigParser()
+    c.add_file(resource_stream(package, "application.ini"))
     xmlconfig(resource_stream(package, "configure.zcml"))
     app=ZC.createObject("Application")
     gsm = ZC.getGlobalSiteManager()
