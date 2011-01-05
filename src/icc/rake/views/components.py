c998b0b907fa3eb2aa015e0733b7724ea8ad5302
Evgeny Cherkashin at center <eugene@irnok.net>
2011-01-05 15:14:29 +0800

    Tried to draw something on the cairo canvas\!

--- src/icc/rake/views/components.py
+++ src/icc/rake/views/components.py
@@ -19,6 +19,8 @@ import icc.rake.models.interfaces as mdli
 import icc.rake.interfaces as ri
 import os
 
+import cairo, math
+
 class Ui:
     pass
 
@@ -197,9 +199,37 @@ class Canvas(View):
     
     def __init__(self, model = None):
         View.__init__(self, model=model)
+        self.ui.main_frame.show_all()
         
     def on_canvas_button_press_event(self, canvas, ev, data=None):
-        print "press:", ev
+        cc = canvas.window.cairo_create()
+        cr = cc
+
+        M_PI=math.pi
+
+        xc = 128.0;
+        yc = 128.0;
+        radius = 100.0;
+        angle1 = 45.0  * (M_PI/180.0);  
+        angle2 = 180.0 * (M_PI/180.0); #  /* in radians           */
+
+        cr.set_line_width (10.0);
+        cr.arc (xc, yc, radius, angle1, angle2);
+        cr.stroke ();
+
+        #/* draw helping lines */
+        cr.set_source_rgba (1, 0.2, 0.2, 0.6);
+        cr.set_line_width (6.0);
+
+        cr.arc (xc, yc, 10.0, 0, 2*M_PI);
+        cr.fill ();
+
+        cr.arc (xc, yc, radius, angle1, angle1);
+        cr.line_to (xc, yc);
+        cr.arc (xc, yc, radius, angle2, angle2);
+        cr.line_to (xc, yc);
+        cr.stroke ();
+        print "press:", ev.x
 
     def on_canvas_button_release_event(self, canvas, ev, user=None):
         print "release:", ev
