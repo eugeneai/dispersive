0c86b85854a4e033c351aceebd06f50cdab1c182
Evgeny Cherkashin at center <eugene@irnok.net>
2011-01-09 01:03:26 +0800

    Graph model basics implementation.

--- src/icc/rake/models/components.py
+++ src/icc/rake/models/components.py
@@ -7,10 +7,6 @@ class Model:
     implements(IModel)
     pass
 
-class Canvas:
-    implements(ICanvas)
-    pass
-
 class Record(object):
     pass
     
@@ -23,7 +19,44 @@ class Module():
     name="<Module>"
     icon=None # Shoul not be here
 
-    
+class Canvas:
+    implements(ICanvas)
 
+    def __init__(self):
+        self.modules={}
+        # connections of the modules
+        self.forwards={}
+        self.backwards={}
+
+    def place(self, module, x, y):
+        self.modules[module]=(x, y)
+
+    def remove(self, module):
+        del self.forwards[module]
+        del self.backwards[module]
+
+    def get_position(self, module):
+        return self.modules[module]
+
+    def connect(self, mfrom, mto):
+        self._add_con(mfrom, mto, self.forwards)
+        self._add_con(mto, mfrom, self.backwards)
+
+    def disconnect(self, mfrom, mto):
+        self._rem_con(mfrom, mto, self.forwards)
+        self._rem_con(mto, mfrom, self.backwards)
+
+    def _add_con(self, mfrom, mto, conns):
+        connl=conns.setdefault(mfrom, [])
+        if connl.find(mto) != -1:
+            connl.append(mto)
+            
+    def _rem_con(self, mfrom, mto, conns):
+        connl=conns.get(mfrom, None)
+        if connl == None:
+            return 
+        idx = connl.find(mto)
+        if idx != -1:
+            del connl[idx]
 
 
