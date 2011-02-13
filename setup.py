b3a6919c0b0c9c3a6331603395fba5bed5ed4c06
Evgeny Cherkashin at center <eugene@irnok.net>
2011-02-13 22:43:52 +0800

    Incremented the vesion. Just for fun.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
     zip_safe = True,
     name="icc.xray",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages("src"),
     package_dir={"": "src"},
     namespace_packages=["icc"],
