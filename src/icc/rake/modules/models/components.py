d34ec98291a67a5588fbf07a691da63de9400507
Evgeny Cherkashin at inca <eugene@irnok.net>
2011-03-29 11:07:34 +0900

    Added forgotten components.

---
+++ src/icc/rake/modules/models/components.py
@@ -0,0 +1,25 @@
+from icc.rake.models.components import Module, OrderedDict
+
+
+class FrameLoadModule(Module):
+    outputs=OrderedDict(data = ('data.frame',))
+    icon='ui/pics/frame_open.svg'
+    name='Data Frame Loading' 
+
+
+class FrameViewModule(Module):
+    inputs=OrderedDict(data = ('data.frame',))
+    icon='ui/pics/frame_view.svg'
+    name='Data Frame Viewing'
+
+
+class LmModule(Module):
+    inputs=OrderedDict(data = ('data.frame',))
+    outputs=OrderedDict(model = ('class.lm',))
+
+class PlotModule(Module):
+    inputs=OrderedDict(x = ('',), y=('',))
+    outputs=OrderedDict()
+    icon='ui/pics/plot.svg'
+    name='Plot (anything)'
+
