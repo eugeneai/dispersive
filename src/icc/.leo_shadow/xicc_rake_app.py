b4fbdec622e34cbe4f1de75d8932ca3b5f3c8b59
Evgeny Cherkashin at center <eugeneai@irnok.net>
2011-12-25 14:52:35 +0900

    Leo shadow updates.

--- src/icc/.leo_shadow/xicc_rake_app.py
+++ src/icc/.leo_shadow/xicc_rake_app.py
@@ -10,8 +10,13 @@ import icc.rake.app as app
 #@nonl
 #@-node:eugeneai.20110116171118.1319:imports
 #@+node:eugeneai.20110116171118.1316:main proceudre
+
+#raise SystemExit, "as"
+
 if __name__=="__main__":
     sys.exit(app.main())
+
+
 
 
 #@-node:eugeneai.20110116171118.1316:main proceudre
--- src/icc/.leo_shadow/xicc_xray_app.py
+++ src/icc/.leo_shadow/xicc_xray_app.py
@@ -10,10 +10,11 @@ import icc.rake.app as app
 #@nonl
 #@-node:eugeneai.20110116171118.1319:imports
 #@+node:eugeneai.20110116171118.1318:main procedure
+
 if __name__=="__main__":
     #@    << run >>
     #@+node:eugeneai.20110116171118.1320:<< run >>
-    sys.exit(app.main('icc.xray '))
+    sys.exit(app.main('icc.xray'))
     #@nonl
     #@-node:eugeneai.20110116171118.1320:<< run >>
     #@nl
