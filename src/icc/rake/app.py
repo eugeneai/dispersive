600a7be306a38a3d6eea470cbe7b397df9949168
Evgeny Cherkashin at center <eugeneai@irnok.net>
2011-12-25 16:43:52 +0900

    User config file support. Application can now save a state to user config upon exit.

--- src/icc/rake/app.py
+++ src/icc/rake/app.py
@@ -11,17 +11,30 @@ import sys
 import cfgparse as cfg
 from icc.rake.interfaces import IConfiguration
 from zope.interface import directlyProvides
+import os.path
 
 #@+node:eugeneai.20110116171118.1425: ** main
 def main(package=None):
     if package == None:
         package=__name__
     c=cfg.ConfigParser()
-    c.add_file(resource_stream(package, "application.ini"))
-    c.parse()
+    config_file=resource_stream(package, "application.ini")
+    c.add_file(config_file)
+    user_conf_opt=c.add_option('user_config_file', keys='app', default=None, type='string')
+    user_config_file=os.path.expanduser(user_conf_opt.get())
+    if user_config_file != None:
+        if os.path.exists(user_config_file):
+            user_conf = c.add_file(user_config_file)
+        else:
+            print "Warning: no user config file '%s' found." % user_config_file
+            user_conf = None
+    else:
+        user_conf=None
+
     gsm = ZC.getGlobalSiteManager()
     directlyProvides(c, IConfiguration)
     conf=c.add_option('conf', default='configuration')
+    c.user_conf=user_conf
     gsm.registerUtility(c)    
     gsm.registerUtility(c, name=conf.get())    
     xmlconfig(resource_stream(package, "configure.zcml"))
@@ -29,7 +42,11 @@ def main(package=None):
     view=c.add_option('view', default='application')
     gsm.registerUtility(app)
     gsm.registerUtility(app, name=view.get())
-    return app.main()
+    rc = app.main()
+    
+    if user_conf: user_conf.write(user_config_file)
+
+    return rc
 
 #@-others
 if __name__ == "__main__":
