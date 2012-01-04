351c05f07735b47a8b2756f86f0ae203e37115d8
Evgeny Cherkasin at center <eugene@irnok.net>
2012-01-04 18:27:05 +0900

    Python master file. But it does not work now.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((python-mode . ((py-master-file . "/path/to/interactivetest.py")
+                 )))
+
+
+;((python-mode . ((py-master-file . "/path/to/interactivetest.py")
+;                 (tags-file-name . "/path/to/TAGS"))))
