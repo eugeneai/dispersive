03803d70e0535386591f26b486af957af774d721
Evgeny Cherkashin <eugene@irnok.net>
2012-04-05 23:50:06 +0900

    Test of the peak detection function with noisy data.

--- src/icc/xray/analysis/tests2.py
+++ src/icc/xray/analysis/tests2.py
@@ -3,7 +3,11 @@ import pylab as p
 import scipy
 print help(scipy.version)
 from scipy.signal import *
-xs = np.arange(0, np.pi, 0.05)
-data = np.sin(xs)
-peakind = find_peaks_cwt(data, np.arange(1,10))
+xs = np.arange(0, np.pi*8, 0.05)
+data = np.sin(xs)+np.sin(xs*400)*10.
+peakind = find_peaks_cwt(data, np.arange(5,6))
 print peakind, xs[peakind],data[peakind]
+p.plot(xs, data)
+for i in peakind:
+    p.axvline(xs[i], color=(0,0,0))
+p.show()
