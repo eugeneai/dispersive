b9b27502892d58ba5c101b2e1999bb572dfd05df
Evgeny Cherkashin <eugene@irnok.net>
2012-05-04 00:08:50 +0900

    Now the numbers of the table are shown.

--- src/icc/xray/pt/ptwidget.py
+++ src/icc/xray/pt/ptwidget.py
@@ -3,22 +3,22 @@ pygtk.require('2.0')
 import gtk, gobject
 import sys
 
-ROWS={
-    ((1,),           (1, (1,))),
-    ((2,),           (1, (18,))),
-    ((3,4),         (2, (1,2))),
-    ((5,10),       (2,(13,18))),
-    ((11,12),     (3, (1,2))),
-    ((13,18),     (3, (13,18))),
-    ((19,36),     (4, (1,18))),
-    ((37,54),     (5, (1,18))),
-    ((55,56),     (6, (1,2))),
-    ((72,86),     (6, (4,18))),
-    ((87,88),     (7, (1,2))),
-    ((104,118), (7, (4,18))),
-    ((57,71),     (9, (4,18))),
-    ((89,103),     (10, (4,18))),
-}
+ROWS=[
+    ((1,),           (1, 1)),
+    ((2,),           (1, 18)),
+    ((3,4),         (2, 1)),
+    ((5,10),       (2, 13)),
+    ((11,12),     (3, 1)),
+    ((13,18),     (3, 13)),
+    ((19,36),     (4, 1)),
+    ((37,54),     (5, 1)),
+    ((55,56),     (6, 1)),
+    ((72,86),     (6, 4)),
+    ((87,88),     (7, 1)),
+    ((104,118), (7, 4)),
+    ((57,71),     (9, 4)),
+    ((89,103),   (10, 4)),
+]
 
 LA = (6,3)
 AC = (7,3)
@@ -29,8 +29,9 @@ class UI:
 class PTWidget(gtk.VBox):
     def __init__(self):
         gtk.VBox.__init__(self)
+        b=gtk.Button("asd")
         self.ui=UI()
-        self.ui.pt=gtk.Table(rows=10, columns=18)
+        self.ui.pt=gtk.Table(rows=10, columns=18, homogeneous=True)
         self.pack_start(self.ui.pt, expand=True, fill=True)
         self.ui.elements=[]
         for i in range(118):
@@ -41,26 +42,30 @@ class PTWidget(gtk.VBox):
                 if len(l) == 1:
                     if l[0]==j:
                         r,c = r
-                        c=c[0]
-                        self.ui.pt.attach(el, c, c+1, r,r+1)
+                        self.ui.pt.attach(el, c-1, c, r-1,r)
                         break
                 else:
-                    if l[0]>=j and l[1]<=j:
+                    if l[0]<=j and j<=l[1]:
                         r, c = r
-                        c=c[j-l[0]]
-                        self.ui.pt.attach(el, c, c+1, r,r+1)
+                        c=c+j-l[0]
+                        self.ui.pt.attach(el, c-1, c, r-1,r)
                         break
                 el.show()
-            self.set_size_request(600,400)
+            self.set_size_request(500,260)
 
+DATA=[]
 
+def import_data(filename):
+    """Import data from a CSV file """
 
-
-if __name__=="__main__":
+def test():
     testw = gtk.Window(gtk.WINDOW_TOPLEVEL)
     testw.connect("destroy", gtk.main_quit)
     pt=PTWidget()
     testw.add(pt)
-    pt.show()
-    testw.show()
+    testw.show_all()
     gtk.main()
+
+if __name__=="__main__":
+    test()
+
