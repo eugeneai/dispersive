e24ab1f5d698e36230d6545bffe01bf9ce5ca89c
Evgeny Cherkashin <eugene@irnok.net>
2012-05-03 23:22:54 +0900

    Buttons layouting.

--- src/icc/xray/pt/ptwidget.py
+++ src/icc/xray/pt/ptwidget.py
@@ -31,6 +31,27 @@ class PTWidget(gtk.VBox):
         gtk.VBox.__init__(self)
         self.ui=UI()
         self.ui.pt=gtk.Table(rows=10, columns=18)
+        self.pack_start(self.ui.pt, expand=True, fill=True)
+        self.ui.elements=[]
+        for i in range(118):
+            j=i+1
+            el=gtk.Button(label=str(j))
+            self.ui.elements.append(el)
+            for l, r in ROWS:
+                if len(l) == 1:
+                    if l[0]==j:
+                        r,c = r
+                        c=c[0]
+                        self.ui.pt.attach(el, c, c+1, r,r+1)
+                        break
+                else:
+                    if l[0]>=j and l[1]<=j:
+                        r, c = r
+                        c=c[j-l[0]]
+                        self.ui.pt.attach(el, c, c+1, r,r+1)
+                        break
+                el.show()
+            self.set_size_request(600,400)
 
 
 
@@ -40,5 +61,6 @@ if __name__=="__main__":
     testw.connect("destroy", gtk.main_quit)
     pt=PTWidget()
     testw.add(pt)
+    pt.show()
     testw.show()
     gtk.main()
