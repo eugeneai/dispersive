8ca1b20074bdcb36f642942b3a36bff05631ae35
Evgeny Cherkashin <eugene@irnok.net>
2012-05-12 22:51:40 +0900

    Updated the databse with line's relative values.

--- src/icc/xray/analysis/lines.py
+++ src/icc/xray/analysis/lines.py
@@ -9,7 +9,7 @@ DEBUG=True
 fields="Z, Line_Name, Comment, line_keV, tube_KV, Filter, Ref_Sample, Ref_Line, Calib, Collimator, Crystal, Detector, Peak_2th, Bkg_2th, LLD, ULD"
 
 CSVLine=namedtuple('CSVLine', fields)
-Line=namedtuple('Line', 'Z, element, name, keV')
+Line=namedtuple('Line', 'Z, element, name, keV, line, rel')
 Element=namedtuple('Element', 'Z, Name')
 
 def _float(x):
@@ -241,7 +241,7 @@ class Lines(object):
         if analytical:
             c.append("((e.Z<=50 and l.name like 'KA%') or (e.Z>50 and l.name like 'LA%'))")
         stmt="""
-        SELECT e.Z, e.Name as element, l.Name as line, l.keV as kev
+        SELECT e.Z, e.Name as element, l.Name as line, l.keV as kev, l.line as line, l.rel as rel
         FROM elements e INNER JOIN lines l ON e.Z=l.Z
         WHERE
         %s
@@ -264,6 +264,28 @@ class Lines(object):
         print ls
         return ls
 
+    def update_rel(self, filename):
+        cur=self.db.cursor()
+        cur.execute("DELETE FROM lines;")
+        self.db.commit();
+        reader=csv.reader(open(filename), delimiter=';')
+        header = reader.next()
+        for row in reader:
+            (ev, el, line, rel, name) = row
+            ev=ev.replace(' ','').replace(',','.')
+            ev=ev.decode('utf8').replace(u'\xa0',u'')
+            #print repr(ev)
+            ev=float(ev)
+            Z, symbol=el.split()
+            rel=int(rel)
+            line=line.decode('utf-8')
+            nrow=(Z, name, ev*1e-3, line, rel)
+            cur.execute('INSERT INTO lines (Z, Name, keV, line, rel) VALUES (?,?,?,?,?)', nrow)
+
+        self.db.commit();
+        print "Update succsessful."
+
+
 
 
 if __name__=='__main__':
@@ -272,13 +294,23 @@ if __name__=='__main__':
     import pprint as pp
     import numpy as np
 
-    lines=Lines(csv='/home/eugeneai/Development/codes/dispersive/data/EdxData1.csv', v=2)
+    if os.name!="nt":
+        ldb=Lines(dbname='/home/eugeneai/Development/codes/dispersive/data/lines.sqlite3')
+    else:
+        ldb=Lines(dbname='C:\\dispersive\\data\\lines.sqlite3')
+
+    #ldb.update_rel('/home/eugeneai/Development/codes/dispersive/data/rel_lines.csv')
+
+    #asd
+    #lines=Lines(csv='/home/eugeneai/Development/codes/dispersive/data/EdxData1.csv', v=2)
     """
     if os.name!="nt":
         lines=Lines(dbname='/home/eugeneai/Development/codes/dispersive/SPECPLUS/DATA/lines.sqlite3')
     else:
         lines=Lines(dbname='C:\\dispersive\\SPECPLUS\\DATA\\lines.sqlite3')
     """
+
+    lines=ldb
 
     L1={'A':0.8, "B":0.8/6.}
     L2={'K':(0,0,0), "L":(1,0,0)}
