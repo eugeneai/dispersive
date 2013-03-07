4398934fc9814fcedd10a731caf9156e18d019bc
Evgeny Cherkashin <eugeneai@irnok.net>
2013-03-07 11:45:41 +0900

    Tried to automate recognition.

--- Bair/fit.R
+++ Bair/fit.R
@@ -2,11 +2,14 @@ funcX <- function(b, X) {
      x0=b[1]
      A=b[2]
      sigma=b[3]
-     exp(-((X-x0)^2/(2*sigma^2)))*sqrt(2*pi)*A
+     s2=2*sigma^2
+     A*exp(-((X-x0)^2/s2))
      }
 
 #source('C:/scaling.R')
 source('./scaling.R')
+DEBUG_PIKES=TRUE
+
 
 dif_spec <- function(b) {
     X=0:(length(CHANNELS)-1)
@@ -22,7 +25,7 @@ opt_spec <- function(x,y, sigma=1.) {
 #CHANNELS=spec[x:y]
 CX0=which.max(CHANNELS)
 CA=CHANNELS[CX0]
-
+print (c(CX0, CA))
 
 pike=optim(c(CX0,CA,sigma), dif_spec, gr=NULL,
    method = c("Nelder-Mead"),
@@ -39,6 +42,14 @@ ZY=funcX(pike$par, X)
 lines(X+x,CHANNELS, type='l', col='blue')
 #lines(ALL_CHANNELS-AZY, col='green')
 lines(X+x, ZY, col='red')
+
+if (DEBUG_PIKES) {
+   png(paste('pike-', CX0, '.png', sep=''))
+   plot(CHANNELS, type='l')
+   lines(ZY, col='red')
+   dev.off()
+}
+
 pike
 }
 
@@ -46,20 +57,40 @@ ALL_CHANNELS=standard
 spec=ALL_CHANNELS[1:3000]
 plot(spec, type='l')
 
-zero_pike=opt_spec(0,150)
-Csigma=zero_pike$par[3]
-
+Csigma=2.
+first = TRUE
 k=3
 while (k>0) {
       X0_pike=which.max(spec)
+      print (X0_pike)
       A_pike=spec[X0_pike]
-      #print(A_pike)
-      x1=(X0_pike-50)
-      y1=(X0_pike+50)
+      print(A_pike)
+      s2=round(Csigma*3.)
+      #x1=(X0_pike-50)
+      #y1=(X0_pike+50)
+      x1=(X0_pike-s2)
+      y1=(X0_pike+s2)
       CHANNELS=spec[x1:y1]
-      spec[x1:y1]=0
+      X=0:(length(CHANNELS)-1)
       #print(spec[x1:y1])
       pike=opt_spec(x1,y1, Csigma)
+      if (first) {
+         Csigma=pike$par[3]
+         if (Csigma<1.) {
+            Csigma = 2.
+         }
+      }
+      print (pike$par)
+      sg=pike$par[3]
+      if (sg<1. || sg>100.) {
+         sg=3.
+      }
+      s4=round(sg*3.5)
+      x1=(X0_pike-s4)
+      y1=(X0_pike+s4)
+      CHANNELS=spec[x1:y1]
+      X=0:(length(CHANNELS)-1)
+      spec[x1:y1]=spec[x1:y1]-funcX(pike$par, X)
       k=k-1
 }
 
