0cd4c83b7bde25c49c5f583966d4980d966497f8
Evgeny Cherkashin <eugene@irnok.net>
2013-09-06 02:20:10 +0900

    Removing debugging from the application.

--- icc_xray_app.sh
+++ icc_xray_app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export LD_PRELOAD="libstdc++.so.6"
+export PYTHONPATH=./:python2/lib:python2/lib/site-packages
 DN=$(dirname $BASH_SOURCE)
-python -u $DN/debug.py $DN/src/icc/icc_xray_app.py $*
+#python -u $DN/debug.py $DN/src/icc/icc_xray_app.py $*
+python -u $DN/src/icc/icc_xray_app.py $*
 
 
