d36bfc95e299e68359b71756bb64a5f1b50f14c0
Evgeny Cherkashin <eugeneai@irnok.net>
2013-09-06 17:44:48 +0900

    Added server launch.

--- icc_xray_app.sh
+++ icc_xray_app.sh
@@ -3,6 +3,8 @@
 #export PYTHONPATH=./:python2/lib:python2/lib/site-packages
 DN=$(dirname $BASH_SOURCE)
 #python -u $DN/debug.py $DN/src/icc/icc_xray_app.py $*
+rpyc_classic.py -p 12211 2>&1 > rpyc.log &
+sleep 1s
 python -u $DN/src/icc/icc_xray_app.py $*
 
 
